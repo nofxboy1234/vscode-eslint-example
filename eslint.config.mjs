3d2f13d666bc087d26977de4f89fd3536ccbb8d2
Dylan Palmboom <nofxboy1234@gmail.com>
2024-04-15 22:42:40 +0200

    Install eslint-config-prettier locally
    It turns off all ESLint rules that are unnecessary
    or might conflict with Prettier.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,8 @@ import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 import pluginJs from "@eslint/js";
 
+import eslintConfigPrettier from "eslint-config-prettier";
+
 // mimic CommonJS variables -- not needed if using CommonJS
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -17,10 +19,12 @@ export default [
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
   { languageOptions: { globals: globals.browser } },
   ...compat.extends("airbnb-base"),
+  eslintConfigPrettier,
   {
     rules: {
-      // 'no-console': 'off',
-      // quotes: ['error', 'double'],
+      // "no-console": "off",
+      // quotes: ["error", "double"],
+      // indent: "error",
     },
   },
 ];
