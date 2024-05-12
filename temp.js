b0a2e7755b938e074541a972067f1ee6b6d48265
Dylan Palmboom <nofxboy1234@gmail.com>
2024-05-12 17:36:20 +0200

    Change callbacks to Promises

--- temp.js
+++ temp.js
@@ -1,27 +1,30 @@
-const userLeft = false;
-const userWatchingCatMeme = true;
+/* eslint-disable */
 
-function watchTutorialCallback(callback, errorCallback) {
-  if (userLeft) {
-    errorCallback({
-      name: 'User left',
-      message: ':(',
-    });
-  } else if (userWatchingCatMeme) {
-    errorCallback({
-      name: 'User watching cat meme',
-      message: 'WebDevSimplified < Cat',
-    });
-  } else {
-    callback('Thumbs up and Subscribe');
-  }
+const userLeft = false;
+const userWatchingCatMeme = false;
+
+function watchTutorialPromise() {
+  return new Promise((resolve, reject) => {
+    if (userLeft) {
+      reject({
+        name: 'User left',
+        message: ':(',
+      });
+    } else if (userWatchingCatMeme) {
+      reject({
+        name: 'User watching cat meme',
+        message: 'WebDevSimplified < Cat',
+      });
+    } else {
+      resolve('Thumbs up and Subscribe');
+    }
+  });
 }
 
-watchTutorialCallback(
-  (message) => {
+watchTutorialPromise()
+  .then((message) => {
     console.log(`Success: ${message}`);
-  },
-  (error) => {
+  })
+  .catch((error) => {
     console.log(`${error.name} ${error.message}`);
-  },
-);
+  });
