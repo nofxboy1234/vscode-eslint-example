73eb18efdf56c990d7b65df92fb02b07523d1b51
Dylan Palmboom <nofxboy1234@gmail.com>
2024-05-13 20:24:19 +0200

    Express x + y example via Promises

--- ch2.js
+++ ch2.js
@@ -1,34 +1,43 @@
 /* eslint-disable */
-function fetchX(callback) {
-  callback(10);
-}
-
-function fetchY(callback) {
-  setTimeout(() => {
-    callback(9);
-  }, 5000);
-}
-
-function add(getX, getY, cb) {
-  var x, y;
-  getX(function (xVal) {
-    x = xVal;
-    // both are ready?
-    if (y != undefined) {
-      cb(x + y); // send along sum
-    }
-  });
-  getY(function (yVal) {
-    y = yVal;
-    // both are ready?
-    if (x != undefined) {
-      cb(x + y); // send along sum
-    }
+function fetchX() {
+  return new Promise((resolve, reject) => {
+    resolve(10);
   });
 }
 
-// `fetchX()` and `fetchY()` are sync or async
-// functions
-add(fetchX, fetchY, function (sum) {
-  console.log(sum); // that was easy, huh?
-});
+function fetchY() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(9);
+    }, 5000);
+  });
+}
+
+function add(xPromise, yPromise) {
+  // `Promise.all([ .. ])` takes an array of promises,
+  // and returns a new promise that waits on them
+  // all to finish
+  return (
+    Promise.all([xPromise, yPromise])
+
+      // when that promise is resolved, let's take the
+      // received `X` and `Y` values and add them together.
+      .then(function (values) {
+        // `values` is an array of the messages from the
+        // previously resolved promises
+        return values[0] + values[1];
+      })
+  );
+}
+
+// `fetchX()` and `fetchY()` return promises for
+// their respective values, which may be ready
+// *now* or *later*.
+add(fetchX(), fetchY())
+  // we get a promise back for the sum of those
+  // two numbers.
+  // now we chain-call `then(..)` to wait for the
+  // resolution of that returned promise.
+  .then(function (sum) {
+    console.log(sum); // that was easier!
+  });
