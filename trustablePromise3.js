9f8001da7f3636cba1e54aed6c841a4dda360619
Dylan Palmboom <nofxboy1234@gmail.com>
2024-05-16 10:32:08 +0200

    Filter through Promise.resolve

--- trustablePromise3.js
+++ trustablePromise3.js
@@ -1,17 +1,16 @@
-/* eslnt-disable */
+/* eslint-disable */
 const p = {
   then(cb, errcb) {
     cb(42);
-    errcb('evil laugh');
+    // errcb('evil laugh');
   },
 };
 
-p.then(
-  (val) => {
+Promise.resolve(p).then(
+  function fulfilled(val) {
     console.log(val); // 42
   },
-  (err) => {
-    // oops, shouldn't have run
-    console.log(err); // evil laugh
+  function rejected(err) {
+    console.error(err);
   },
 );
