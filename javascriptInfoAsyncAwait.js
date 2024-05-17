eafdc084fdff04a24b58bb39ca83ff5920e7d489
Dylan Palmboom <nofxboy1234@gmail.com>
2024-05-17 14:46:04 +0200

    Update

--- javascriptInfoAsyncAwait.js
+++ javascriptInfoAsyncAwait.js
@@ -1,11 +1,26 @@
-async function f() {
-  const promise = new Promise((resolve, reject) => {
-    setTimeout(() => resolve('done!'), 1000);
-  });
+async function showAvatar() {
+  // read our JSON
+  const response = await fetch('/article/promise-chaining/user.json');
+  const user = await response.json();
 
-  const result = await promise; // wait until the promise resolves (*)
+  // read github user
+  const githubResponse = await fetch(
+    `https://api.github.com/users/${user.name}`,
+  );
+  const githubUser = await githubResponse.json();
 
-  console.log(result); // "done!"
+  // show the avatar
+  const img = document.createElement('img');
+  img.src = githubUser.avatar_url;
+  img.className = 'promise-avatar-example';
+  document.body.append(img);
+
+  // wait 3 seconds
+  await new Promise((resolve, reject) => setTimeout(resolve, 3000));
+
+  img.remove();
+
+  return githubUser;
 }
 
-f();
+showAvatar();
