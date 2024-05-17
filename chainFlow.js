9a34b1b0cb082832ad55fe4d60aa8cad7765a38f
Dylan Palmboom <nofxboy1234@gmail.com>
2024-05-17 10:53:51 +0200

    Introduce asynchrony to the wrapping Promise

--- chainFlow.js
+++ chainFlow.js
@@ -4,11 +4,15 @@ var p = Promise.resolve(21);
 p.then(function (v) {
   console.log(v); // 21
 
-  // create a promise and return it
+  // create a promise to return
   return new Promise(function (resolve, reject) {
-    // fulfill with value `42`
-    resolve(v * 2);
+    // introduce asynchrony!
+    setTimeout(function () {
+      // fulfill with value `42`
+      resolve(v * 2);
+    }, 100);
   });
 }).then(function (v) {
+  // runs after the 100ms delay in the previous step
   console.log(v); // 42
 });
