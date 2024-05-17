389c38273076648c68bde2c379a44fe7ebb2a957
Dylan Palmboom <nofxboy1234@gmail.com>
2024-05-17 11:32:40 +0200

    Test `assumed rejection handler`

--- chainFlow.js
+++ chainFlow.js
@@ -1,42 +1,15 @@
 /* eslint-disable */
-// assume an `ajax( {url}, {callback} )` utility
+var p = new Promise(function (resolve, reject) {
+  reject('Oops');
+});
 
-// Promise-aware ajax
-function request(url) {
-  return new Promise(function (resolve, reject) {
-    // the `ajax(..)` callback should be our
-    // promise's `resolve(..)` function
-
-    // ajax(url, resolve);
-    setTimeout(() => {
-      resolve();
-    }, 2000);
-  });
-}
-
-// step 1:
-request('http://some.url.1/')
-  // step 2:
-  .then(function (response1) {
-    foo.bar(); // undefined, error!
-
+var p2 = p.then(
+  function fulfilled() {
     // never gets here
-    return request('http://some.url.2/?v=' + response1);
-  })
-
-  // step 3:
-  .then(
-    function fulfilled(response2) {
-      // never gets here
-    },
-    // rejection handler to catch the error
-    function rejected(err) {
-      console.log(err); // `TypeError` from `foo.bar()` error
-      return 42;
-    },
-  )
-
-  // step 4:
-  .then(function (msg) {
-    console.log(msg); // 42
-  });
+  },
+  // assumed rejection handler, if omitted or
+  // any other non-function value passed
+  // function(err) {
+  //     throw err;
+  // }
+);
