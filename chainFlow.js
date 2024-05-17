a62e7fc2e61464f202e350e31b2e728ddf97361f
Dylan Palmboom <nofxboy1234@gmail.com>
2024-05-17 11:24:48 +0200

    Test out getting an error in one of the steps of the Promise chain

--- chainFlow.js
+++ chainFlow.js
@@ -6,14 +6,37 @@ function request(url) {
   return new Promise(function (resolve, reject) {
     // the `ajax(..)` callback should be our
     // promise's `resolve(..)` function
-    ajax(url, resolve);
+
+    // ajax(url, resolve);
+    setTimeout(() => {
+      resolve();
+    }, 2000);
   });
 }
 
+// step 1:
 request('http://some.url.1/')
+  // step 2:
   .then(function (response1) {
+    foo.bar(); // undefined, error!
+
+    // never gets here
     return request('http://some.url.2/?v=' + response1);
   })
-  .then(function (response2) {
-    console.log(response2);
+
+  // step 3:
+  .then(
+    function fulfilled(response2) {
+      // never gets here
+    },
+    // rejection handler to catch the error
+    function rejected(err) {
+      console.log(err); // `TypeError` from `foo.bar()` error
+      return 42;
+    },
+  )
+
+  // step 4:
+  .then(function (msg) {
+    console.log(msg); // 42
   });
