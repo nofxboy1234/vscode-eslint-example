4bafcf662dd4ef70fa33dbb9c236266f2362b27e
Dylan Palmboom <nofxboy1234@gmail.com>
2024-05-17 10:47:45 +0200

    Chain .then()

--- chainFlow.js
+++ chainFlow.js
@@ -1,14 +1,13 @@
 /* eslint-disable */
 var p = Promise.resolve(21);
 
-var p2 = p.then(function (v) {
+p.then(function (v) {
   console.log(v); // 21
 
-  // fulfill `p2` with value `42`
+  // fulfill the chained promise with value `42`
   return v * 2;
-});
-
-// chain off `p2`
-p2.then(function (v) {
-  console.log(v); // 42
-});
+})
+  // here's the chained promise
+  .then(function (v) {
+    console.log(v); // 42
+  });
