cdbe7ad3b9d594a96eca614c0f9c23d8514f6361
Dylan Palmboom <nofxboy1234@gmail.com>
2024-05-20 16:59:37 +0200

    Test out async class method

--- async_functions.js
+++ async_functions.js
@@ -1,19 +1,8 @@
-class Thenable {
-  constructor(num) {
-    this.num = num;
-  }
-
-  then(resolve, reject) {
-    console.log(resolve);
-    // resolve with this.num * 2 after 1000ms
-    setTimeout(() => resolve(this.num * 2), 1000); // (*)
+class Waiter {
+  async wait() {
+    return await Promise.resolve(1);
   }
 }
 
-async function hello() {
-  // waits for 1 second, the result becomes 2
-  const result = await new Thenable(1);
-  console.log(result);
-}
-
-hello();
+new Waiter().wait().then(console.log);
+// new Waiter().wait().then((result) => console.log(result));
