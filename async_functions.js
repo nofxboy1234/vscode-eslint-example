31e1b84053bdd45f7350731b8ea45b680fcf3e8d
Dylan Palmboom <nofxboy1234@gmail.com>
2024-05-20 15:42:34 +0200

    Compare async functions vs promise executors

--- async_functions.js
+++ async_functions.js
@@ -1,5 +1,25 @@
+const doStuff = async () => 22;
+// const doStuff = async () => {
+//   throw new Error('goodbye error!');
+// };
+
 const fetcher = async function fetcher(url) {
-  await new Promise((resolve, reject) => setTimeout(resolve, 2000));
+  try {
+    const value1 = await doStuff();
+    console.log(`value1: ${value1}`);
+    const value2 = await new Promise((resolve, reject) => {
+      setTimeout(() => {
+        console.log('timed out');
+        resolve(19);
+        // return 19; - does not work
+        // reject(new Error('hello error!'));
+        // throw new 0Error('hello error!'); - does not work
+      }, 2000);
+    });
+    console.log(`value2: ${value2}`);
+  } catch (error) {
+    console.error(error);
+  }
   const response = {
     async json() {
       return url;
