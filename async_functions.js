e724213e02240f915fddafc6b0453c354724651f
Dylan Palmboom <nofxboy1234@gmail.com>
2024-05-20 12:59:20 +0200

    Add showAvatar() example form Promises Chaining chapter

--- async_functions.js
+++ async_functions.js
@@ -1,6 +1,28 @@
-function f() {
-  const promise = Promise.resolve(1);
-  let result = await promise;
+function loadJson(url) {
+  return fetch(url).then((response) => response.json());
 }
 
-f();
+function loadGithubUser(name) {
+  return loadJson(`https://api.github.com/users/${name}`);
+}
+
+function showAvatar(githubUser) {
+  return new Promise((resolve, reject) => {
+    const img = document.createElement('img');
+    img.src = githubUser.avatar_url;
+    img.className = 'promise-avatar-example';
+    document.body.append(img);
+
+    setTimeout(() => {
+      img.remove();
+      resolve(githubUser);
+    }, 3000);
+  });
+}
+
+// Use them:
+loadJson('/article/promise-chaining/user.json')
+  .then((user) => loadGithubUser(user.name))
+  .then(showAvatar)
+  .then((githubUser) => alert(`Finished showing ${githubUser.name}`));
+// ...
