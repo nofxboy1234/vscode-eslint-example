04c1b404b6aae0cb6d52048f1f0c09f50a4123d2
Dylan Palmboom <nofxboy1234@gmail.com>
2024-05-20 11:36:29 +0200

    Test out async function used for array forEach and map

--- asyncFunctions.js
+++ asyncFunctions.js
@@ -1,79 +1,35 @@
-const { setTimeout: setTimeoutPromiseBased } = require('node:timers/promises');
 /* eslint-disable */
 
-function promisifyTimeout() {
-  return new Promise((resolve) => {
-    // do something asynchronously and return a promise
+function double(number) {
+  return new Promise((resolve, rejects) => {
     setTimeout(() => {
-      resolve('hello');
+      resolve(number * 2);
     }, 2000);
   });
 }
 
-function promisifyLog() {
-  return new Promise((resolve, reject) => {
-    // do something asynchronously and return a promise
-    promisifyTimeout().then((message) => {
-      console.log(`message is: ${message}`);
-      resolve();
-    });
+function triple(number) {
+  return new Promise((resolve, rejects) => {
+    setTimeout(() => {
+      resolve(number * 3);
+    }, 2000);
   });
 }
 
-const yourAsyncFunction2 = async () => {
-  // do something asynchronously and return a promise
-  // return await promisifyTimeout();
+const anArray = [3, 6, 9];
 
-  // const result = await promisifyTimeout();
-  // return result;
-
-  // return promisifyTimeout();
-
-  const promise = promisifyLog();
-  try {
-    await promise;
-    await promise;
-  } catch (error) {
-    console.log(error);
-  }
-
-  // await promisifyLog();
-  console.log('here');
-
-  // promisifyLog().then(() => {
-  //   console.log('here');
-  // });
-
-  // const message = await setTimeoutPromiseBased(2000, 'hello');
-  // console.log(`message is: ${message}`);
-
-  // const message = await setTimeoutPromiseBased(2000, () => {
-  //   console.log('hey');
-  // });
-  // message();
-
-  return promisifyTimeout();
-
-  // setTimeout(() => {
-  //   return 'hello';
-  // }, 2000);
-};
-
-yourAsyncFunction2().then((result) => {
-  console.log(`result is: ${result}`);
+anArray.forEach(async (item) => {
+  // do something asynchronously for each item in 'anArray'
+  const doubled = await double(item);
+  console.log(doubled);
 });
 
-// const anArray = ['a', 'b', 'c'];
-// anArray.forEach(async (item) => {
-//   // do something asynchronously for each item in 'anArray'
-//   setTimeout(() => {
-//     console.log(item);
-//   }, 2000);
-//   // one could also use .map here to return an array of promises to use with 'Promise.all()'
-// });
-
-// server.getPeople().then(async (people) => {
-//   people.forEach((person) => {
-//     // do something asynchronously for each person
-//   });
-// });
+const tripledArrray = anArray.map(async (item) => {
+  // one could also use .map here to return an array of promises to use with 'Promise.all()'
+  const tripled = await triple(item);
+  return tripled;
+});
+console.log(tripledArrray);
+Promise.all(tripledArrray).then((results) => {
+  console.log(results);
+});
