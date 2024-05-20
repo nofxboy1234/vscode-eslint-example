bcdc10f95d57086dd1b99fe919809cb91ffcce97
Dylan Palmboom <nofxboy1234@gmail.com>
2024-05-20 15:05:14 +0200

    Wrap async-await into an anonymouse IIFE

--- async_functions.js
+++ async_functions.js
@@ -1,26 +1,15 @@
-async function showAvatar() {
-  // read our JSON
-  const response = await fetch('/article/promise-chaining/user.json');
+const fetcher = async function fetcher(url) {
+  await new Promise((resolve, reject) => setTimeout(resolve, 2000));
+  const response = {
+    async json() {
+      return url;
+    },
+  };
+  return response;
+};
+
+(async () => {
+  const response = await fetcher('/article/promise-chaining/user.json');
   const user = await response.json();
-
-  // read github user
-  const githubResponse = await fetch(
-    `https://api.github.com/users/${user.name}`,
-  );
-  const githubUser = await githubResponse.json();
-
-  // show the avatar
-  const img = document.createElement('img');
-  img.src = githubUser.avatar_url;
-  img.className = 'promise-avatar-example';
-  document.body.append(img);
-
-  // wait 3 seconds
-  await new Promise((resolve, reject) => setTimeout(resolve, 3000));
-
-  img.remove();
-
-  return githubUser;
-}
-
-showAvatar();
+  console.log(user);
+})();
