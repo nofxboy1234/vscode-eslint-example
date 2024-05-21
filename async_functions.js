7e6abdcfaf772aa1bef3ec9519fc361466831386
Dylan Palmboom <nofxboy1234@gmail.com>
2024-05-21 17:35:31 +0200

    Update yolo functions

--- async_functions.js
+++ async_functions.js
@@ -1,10 +1,30 @@
 // Create a function without any error handling
-async function yolo() {
+async function asyncYolo() {
   // do something that errors out
+  await new Promise((resolve, reject) => setTimeout(resolve, 3000));
   const wes = await fetch('https://no.com');
 }
 
-yolo()
+function yolo() {
+  // do something that errors out
+  return new Promise((resolve, reject) => {
+    new Promise((resolve, reject) => setTimeout(resolve, 3000)).then(() => {
+      fetch('https://no.com')
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error('response was not ok!');
+          }
+          console.log(`response: ${response}`);
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    });
+  });
+}
+
+const promise = yolo();
+promise
   .then((message) => {
     console.log(message);
   })
