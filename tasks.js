09af6508d62dbcc18d7054c4935067d4aec0d942
Dylan Palmboom <nofxboy1234@gmail.com>
2024-05-21 13:37:45 +0200

    Change code to use async await

--- tasks.js
+++ tasks.js
@@ -1,16 +1,14 @@
-function loadJson(url) {
-  return fetch(url).then((response) => {
-    if (response.status == 200) {
-      return response.json();
-    }
-    throw new Error(response.status);
-  });
+async function loadJson(url) {
+  const response = await fetch(url);
+
+  if (response.status === 200) {
+    const json = await response.json();
+    return json;
+  }
+
+  throw new Error(response.status);
 }
 
 loadJson('https://javascript.info/no-such-user.json').catch((err) => {
   console.error(err);
 }); // Error: 404
-
-// (async () => {
-//   await fetch(url)
-// })();
