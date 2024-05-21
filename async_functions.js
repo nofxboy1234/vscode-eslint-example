d6c82b82756a5c63623bd855c6ad0c4cb1b65a79
Dylan Palmboom <nofxboy1234@gmail.com>
2024-05-21 16:07:41 +0200

    Add sleep function

---
+++ async_functions.js
@@ -0,0 +1,16 @@
+function sleep(amount) {
+  return new Promise((resolve, reject) => {
+    if (amount <= 300) {
+      return reject('That is too fast, cool it down!');
+    }
+    setTimeout(() => resolve(`Slept for ${amount}`), amount);
+  });
+}
+
+sleep(2000)
+  .then((message) => {
+    console.log(message);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
