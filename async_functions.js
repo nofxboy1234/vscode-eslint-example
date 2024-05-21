560cd8724af3089a198d3fe496ce072b26fe7b31
Dylan Palmboom <nofxboy1234@gmail.com>
2024-05-21 11:54:48 +0200

    Update

--- async_functions.js
+++ async_functions.js
@@ -1,15 +1,21 @@
+// jasync function getValue() {}
+
 // async function getValue() {
 //   return 1;
 // }
-// async function getValue() {}
-// const getValue = async () => 1;
-// const getValue = async () => {};
+
+// function getValue() {
+//   return new Promise((resolve, reject) => {});
+// }
+
 function getValue() {
-  return new Promise((resolve, reject) => {});
+  return new Promise((resolve, reject) => {
+    resolve(1);
+  });
 }
 
 (async () => {
   const promise = getValue();
   const response = await promise;
-  console.log('Done!');
+  console.log(response);
 })();
