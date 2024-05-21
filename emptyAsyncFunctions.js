fd00c6c9e6ca469842672cf9fe7b477fb75ebd06
Dylan Palmboom <nofxboy1234@gmail.com>
2024-05-21 12:34:12 +0200

    Update emptyAsyncFunctions

--- emptyAsyncFunctions.js
+++ emptyAsyncFunctions.js
@@ -1,15 +1,49 @@
+async function getValue() {}
+
+// async function getValue() {
+//   return undefined;
+// }
+
+// const getValue = async () => undefined;
+
+// const getValue = async () => {};
+
 // async function getValue() {
 //   return 1;
 // }
-// async function getValue() {}
-// const getValue = async () => 1;
-// const getValue = async () => {};
-function getValue() {
-  return new Promise((resolve, reject) => {});
-}
+
+// async function getValue() {
+//   throw new Error('promise failed');
+// }
+
+// function getValue() {
+//   return new Promise((resolve, reject) => {});
+// }
+
+// function getValue() {
+//   return new Promise((resolve, reject) => {
+//     resolve(undefined);
+//   });
+// }
+
+// function getValue() {
+//   return new Promise((resolve, reject) => {
+//     resolve(1);
+//   });
+// }
+
+// function getValue() {
+//   return new Promise((resolve, reject) => {
+//     reject(new Error('promise failed!'));
+//   });
+// }
 
 (async () => {
-  const promise = getValue();
-  const response = await promise;
-  console.log('Done!');
+  try {
+    const promise = getValue();
+    const response = await promise;
+    console.log(response);
+  } catch (err) {
+    console.error(err);
+  }
 })();
