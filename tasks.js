d59d718b4d5552eba0cfa0a8e85623c93003c9bc
Dylan Palmboom <nofxboy1234@gmail.com>
2024-05-21 14:05:20 +0200

    Update tasks

--- tasks.js
+++ tasks.js
@@ -1,41 +1,16 @@
-class HttpError extends Error {
-  constructor(response) {
-    super(`${response.status} for ${response.url}`);
-    this.name = 'HttpError';
-    this.response = response;
-  }
+async function wait() {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  return 10;
 }
 
-async function loadJson(url) {
-  const response = await fetch(url);
-  if (response.status == 200) {
-    return response.json();
-  }
-  throw new HttpError(response);
+function f() {
+  // ...what should you write here?
+  // we need to call async wait() and wait to get 10
+  // remember, we can't use "await"
+  wait().then((value) => {
+    console.log(value);
+  });
 }
 
-// Ask for a user name until github returns a valid user
-async function demoGithubUser() {
-  let user;
-  while (true) {
-    const name = prompt('Enter a name?', 'iliakan');
-
-    try {
-      user = await loadJson(`https://api.github.com/users/${name}`);
-      break; // no error, exit loop
-    } catch (err) {
-      if (err instanceof HttpError && err.response.status == 404) {
-        // loop continues after the alert
-        console.log('No such user, please reenter.');
-      } else {
-        // unknown error, rethrow
-        throw err;
-      }
-    }
-  }
-
-  console.log(`Full name: ${user.name}.`);
-  return user;
-}
-
-demoGithubUser();
+f();
