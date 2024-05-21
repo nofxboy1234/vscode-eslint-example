b0ecb3b5f00aa640257e879868e45456b51d3091
Dylan Palmboom <nofxboy1234@gmail.com>
2024-05-21 13:11:25 +0200

    Update

--- async_functions.js
+++ async_functions.js
@@ -1,12 +1,13 @@
-async function hello() {
-  const response = await fetch('http://no-such-url');
-}
-
 let promise;
 (async () => {
-  hello().catch((err) => {
-    console.error(err);
+  // wait for the array of results
+  const results = await Promise.all([
+    fetch('http://no-such-url'),
+    fetch('http://no-such-url2'),
+  ]).catch((err) => {
+    console.log(err);
   });
+
   // try {
   //   promise = hello();
   //   const response = await promise;
