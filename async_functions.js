555638c03363b23eebb0a2354dfdbc5d57ebe6f2
Dylan Palmboom <nofxboy1234@gmail.com>
2024-05-21 12:29:04 +0200

    Compare regular functions with arrow functions when function is empty

--- async_functions.js
+++ async_functions.js
@@ -1,21 +1,49 @@
-// jasync function getValue() {}
+async function getValue() {}
+
+// async function getValue() {
+//   return undefined;
+// }
+
+// const getValue = async () => undefined;
+
+// const getValue = async () => {};
 
 // async function getValue() {
 //   return 1;
+// }
+
+// async function getValue() {
+//   throw new Error('promise failed');
 // }
 
 // function getValue() {
 //   return new Promise((resolve, reject) => {});
 // }
 
-function getValue() {
-  return new Promise((resolve, reject) => {
-    resolve(1);
-  });
-}
+// function getValue() {
+//   return new Promise((resolve, reject) => {
+//     resolve(undefined);
+//   });
+// }
+
+// function getValue() {
+//   return new Promise((resolve, reject) => {
+//     resolve(1);
+//   });
+// }
+
+// function getValue() {
+//   return new Promise((resolve, reject) => {
+//     reject(new Error('promise failed!'));
+//   });
+// }
 
 (async () => {
-  const promise = getValue();
-  const response = await promise;
-  console.log(response);
+  try {
+    const promise = getValue();
+    const response = await promise;
+    console.log(response);
+  } catch (err) {
+    console.error(err);
+  }
 })();
