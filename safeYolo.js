8bad278a482de32c3c21686408096a0fce1e0b22
Dylan Palmboom <nofxboy1234@gmail.com>
2024-05-21 18:45:09 +0200

    Implement safeYolo with handleError

---
+++ safeYolo.js
@@ -0,0 +1,23 @@
+/* eslint-disable */
+
+function handleError(fn) {
+  return function (...params) {
+    return fn(...params).catch(function (err) {
+      // do something with the error!
+      console.error(`Oops!`, err);
+    });
+  };
+}
+
+async function fetcher() {
+  throw new Error('Fetcher error!');
+}
+
+// Create a function without any error handling
+async function yolo() {
+  // do something that errors out
+  const response = await fetcher('https://no.com');
+}
+
+const safeYolo = handleError(yolo);
+safeYolo();
