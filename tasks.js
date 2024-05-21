b6ce41b23250f53321203cbb8f3724583b3e8c36
Dylan Palmboom <nofxboy1234@gmail.com>
2024-05-21 13:41:45 +0200

    Update tasks

--- tasks.js
+++ tasks.js
@@ -1,14 +1,36 @@
-async function loadJson(url) {
-  const response = await fetch(url);
-
-  if (response.status === 200) {
-    const json = await response.json();
-    return json;
+class HttpError extends Error {
+  constructor(response) {
+    super(`${response.status} for ${response.url}`);
+    this.name = 'HttpError';
+    this.response = response;
   }
-
-  throw new Error(response.status);
 }
 
-loadJson('https://javascript.info/no-such-user.json').catch((err) => {
-  console.error(err);
-}); // Error: 404
+function loadJson(url) {
+  return fetch(url).then((response) => {
+    if (response.status == 200) {
+      return response.json();
+    }
+    throw new HttpError(response);
+  });
+}
+
+// Ask for a user name until github returns a valid user
+function demoGithubUser() {
+  const name = prompt('Enter a name?', 'iliakan');
+
+  return loadJson(`https://api.github.com/users/${name}`)
+    .then((user) => {
+      console.log(`Full name: ${user.name}.`);
+      return user;
+    })
+    .catch((err) => {
+      if (err instanceof HttpError && err.response.status == 404) {
+        console.log('No such user, please reenter.');
+        return demoGithubUser();
+      }
+      throw err;
+    });
+}
+
+demoGithubUser();
