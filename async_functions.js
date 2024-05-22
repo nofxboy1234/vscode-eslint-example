5e9b4a79b142603f7c7e44171c69e21187f3ca63
Dylan Palmboom <nofxboy1234@gmail.com>
2024-05-22 10:15:02 +0200

    Update

--- async_functions.js
+++ async_functions.js
@@ -1,34 +1,34 @@
 /* eslint-disable */
 
-// const randomResponse = async () => {
-//   // await '';
-//   // return 22;
-//   // throw new Error('Error!');
-//   // let message = '';
-//   // const num = Math.floor(Math.random() * 10) + 1;
-//   // if (num < 5) {
-//   //   message = 'You guessed correctly!';
-//   //   // message = await 'You guessed correctly!';
-//   //   console.log(message);
-//   // } else {
-//   //   message = 'Better luck next time...';
-//   //   // message = await 'Better luck next time...';
-//   //   console.log(message);
-//   // }
-// };
+const randomResponse = async () => {
+  await '';
+  // return 22;
+  // throw new Error('Error!');
+  // let message = '';
+  // const num = Math.floor(Math.random() * 10) + 1;
+  // if (num < 5) {
+  //   message = 'You guessed correctly!';
+  //   // message = await 'You guessed correctly!';
+  //   console.log(message);
+  // } else {
+  //   message = 'Better luck next time...';
+  //   // message = await 'Better luck next time...';
+  //   console.log(message);
+  // }
+};
 
-const randomResponse = () =>
-  new Promise((resolve, reject) => {
-    let message = '';
-    const num = Math.floor(Math.random() * 10) + 1;
-    if (num < 5) {
-      message = 'You guessed correctly!';
-      console.log(message);
-    } else {
-      message = 'Better luck next time...';
-      console.log(message);
-    }
-  });
+// const randomResponse = () =>
+//   new Promise((resolve, reject) => {
+//     let message = '';
+//     const num = Math.floor(Math.random() * 10) + 1;
+//     if (num < 5) {
+//       message = 'You guessed correctly!';
+//       console.log(message);
+//     } else {
+//       message = 'Better luck next time...';
+//       console.log(message);
+//     }
+//   });
 
 // const promise = randomResponse();
 // promise
@@ -43,7 +43,7 @@ const randomResponse = () =>
   try {
     const promise = randomResponse();
     const result = await promise;
-    
+
     console.log(result);
   } catch (err) {
     console.error(err);
