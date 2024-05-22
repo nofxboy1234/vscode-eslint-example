2369acf2e0212d7826a5dfc28f728f2983f05d6f
Dylan Palmboom <nofxboy1234@gmail.com>
2024-05-22 09:47:57 +0200

    Update randomResponse functions

--- async_functions.js
+++ async_functions.js
@@ -1,13 +1,35 @@
-const randomResponse = async () => {
-  let message = '';
-  const num = Math.floor(Math.random() * 10) + 1;
-  if (num < 5) {
-    message = await 'You guessed correctly!';
-    console.log(message);
-  } else {
-    message = await 'Better luck next time...';
-    console.log(message);
-  }
-};
+// const randomResponse = async () => {
+//   let message = '';
+//   const num = Math.floor(Math.random() * 10) + 1;
+//   if (num < 5) {
+//     // message = 'You guessed correctly!';
+//     message = await 'You guessed correctly!';
+//     console.log(message);
+//   } else {
+//     message = 'Better luck next time...';
+//     // message = await 'Better luck next time...';
+//     console.log(message);
+//   }
+// };
 
-randomResponse();
+const randomResponse = () =>
+  new Promise((resolve, reject) => {
+    let message = '';
+    const num = Math.floor(Math.random() * 10) + 1;
+    if (num < 5) {
+      message = 'You guessed correctly!';
+      console.log(message);
+    } else {
+      message = 'Better luck next time...';
+      console.log(message);
+    }
+  });
+
+const promise = randomResponse();
+promise
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
