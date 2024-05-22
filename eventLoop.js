6c877d0eca2f4d21639d4a60624f9ed61b16bf43
Dylan Palmboom <nofxboy1234@gmail.com>
2024-05-22 13:46:05 +0200

    Add eventLoop

---
+++ eventLoop.js
@@ -0,0 +1,15 @@
+/* eslint-disable */
+async function timer() {
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  return 'Done!';
+}
+
+timer().then((message) => {
+  console.log(message);
+});
+
+console.log('Carrying on!');
+
+setTimeout(console.log, 3000, 'hello');
+
+console.log('End!');
