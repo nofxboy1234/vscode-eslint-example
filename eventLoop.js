891c789d4c9fb416324b1f0c7876fdd4259e2f42
Dylan Palmboom <nofxboy1234@gmail.com>
2024-05-22 14:01:21 +0200

    Update eventLoop file

--- eventLoop.js
+++ eventLoop.js
@@ -1,15 +1,16 @@
 /* eslint-disable */
-async function timer() {
-  await new Promise((resolve) => setTimeout(resolve, 3000));
-  return 'Done!';
-}
+// async function timer() {
+//   await new Promise((resolve) => setTimeout(resolve, 3000));
+//   return 'Done!';
+// }
 
-timer().then((message) => {
-  console.log(message);
-});
+// timer().then((message) => {
+//   console.log(message);
+// });
 
-console.log('Carrying on!');
+// console.log('Carrying on!');
+// setTimeout(console.log, 3000, 'hello');
+// console.log('End!');
 
-setTimeout(console.log, 3000, 'hello');
-
-console.log('End!');
+Promise.resolve().then(() => console.log('Hey!'));
+console.log('Yo!');
