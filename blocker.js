d9933623edc8837cc0ccef64c599a97cb668292c
Dylan Palmboom <nofxboy1234@gmail.com>
2024-05-23 13:23:47 +0200

    Wrap code in a Promise
    The creation of the Promise and the big While loop
    is still happening on the Main Thread.
    
    Only the resolving of the value happens as a MicroTask.

--- blocker.js
+++ blocker.js
@@ -3,12 +3,20 @@ const tick = Date.now();
 const log = (v) => console.log(`${v} \n Elapsed: ${Date.now() - tick}ms`);
 
 const codeBlocker = () => {
-  let i = 0;
-  while (i < 1000000000) {
-    i++;
-  }
+  // let i = 0;
+  // while (i < 1000000000) {
+  //   i++;
+  // }
 
-  return 'billion loops done';
+  // return 'billion loops done';
+
+  return new Promise((resolve, reject) => {
+    let i = 0;
+    while (i < 1000000000) {
+      i++;
+    }
+    resolve('billion loops done');
+  });
 };
 
 log('Synchronous 1');
