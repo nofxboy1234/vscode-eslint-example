7f06a01bc3736fa41eb057b283ce763dea976a29
Dylan Palmboom <nofxboy1234@gmail.com>
2024-05-23 14:06:15 +0200

    Use .then() to show log

--- blocker.js
+++ blocker.js
@@ -3,12 +3,16 @@ const tick = Date.now();
 const log = (v) => console.log(`${v} \n Elapsed: ${Date.now() - tick}ms`);
 
 const codeBlocker = () => {
+  // Blocking
+
   // let i = 0;
   // while (i < 1000000000) {
   //   i++;
   // }
 
   // return 'billion loops done';
+
+  // Async blocking
 
   return new Promise((resolve, reject) => {
     let i = 0;
@@ -21,6 +25,6 @@ const codeBlocker = () => {
 
 log('Synchronous 1');
 
-log(codeBlocker());
+codeBlocker().then(log);
 
 log('Synchronous 2');
