58284abad2578a609c59287a6ea71febf2c35937
Dylan Palmboom <nofxboy1234@gmail.com>
2024-05-23 12:31:51 +0200

    Test Synchronous and Asynchronous code
    Async code:
    - setTimeout of 0
    - Promise.resolve()
    
    MacroTasks (Executed on the next event loop): setTimeout, setInterval
    MicroTasks (Executed before the start of the next event loop): Fullfilled Promise

---
+++ async_await.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+
+console.log('Synchronous 1');
+
+setTimeout((_) => console.log('Timeout 2'), 0);
+
+Promise.resolve().then((_) => console.log('Promise'));
+
+console.log('Synchronous 4');
