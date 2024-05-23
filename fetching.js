4c13484e8617765d8c9536ee50fbd800ca02d86c
Dylan Palmboom <nofxboy1234@gmail.com>
2024-05-23 12:58:40 +0200

    Add .catch()

--- fetching.js
+++ fetching.js
@@ -2,6 +2,9 @@
 
 const promise = fetch('https://jsonplaceholder.typicode.com/todos/1');
 
-promise.then((res) => res.json()).then((user) => console.log(user.title));
+promise
+  .then((res) => res.json())
+  .then((user) => console.log(user.title))
+  .catch((err) => console.error(err));
 
 console.log('Synchronous');
