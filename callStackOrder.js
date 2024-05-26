307e5cbb1dc89ad5ca25ae1e0b90f45a3119832e
Dylan Palmboom <nofxboy1234@gmail.com>
2024-05-26 11:44:12 +0200

    Add time logger

--- callStackOrder.js
+++ callStackOrder.js
@@ -1,8 +1,11 @@
 /* eslint-disable */
-const foo = () => console.log('First');
-const zoo = () => setTimeout(() => console.log('Fourth'), 1000);
-const bar = () => setTimeout(() => console.log('Third'), 500);
-const baz = () => console.log('Second');
+const startTime = Date.now();
+const log = (v) => console.log(`${v} - Elapsed: ${Date.now() - startTime}ms`);
+
+const foo = () => log('First');
+const zoo = () => setTimeout(() => log('Fourth'), 1000);
+const bar = () => setTimeout(() => log('Third'), 500);
+const baz = () => log('Second');
 
 bar();
 zoo();
