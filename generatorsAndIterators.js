3398e1143e77620a3ff06a533b7b921f85686c26
Dylan Palmboom <nofxboy1234@gmail.com>
2024-05-28 14:45:33 +0200

    Add generatorsAndIterators.js

---
+++ generatorsAndIterators.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+function normalFunction() {
+  console.log('I am the coolest function!');
+  console.log('There is no way of stopping me');
+  console.log('Oh boi we are still going');
+  console.log('Okay finally all done now');
+}
+
+normalFunction();
