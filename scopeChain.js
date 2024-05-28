01f779fe97cbfcfdabe43796e8980362f13c5cb7
Dylan Palmboom <nofxboy1234@gmail.com>
2024-05-28 09:58:59 +0200

    Add scopeChain.js

---
+++ scopeChain.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+const name = 'Lydia';
+const age = 21;
+const city = 'San Francisco';
+
+function getPersonInfo() {
+  const name = 'Sarah';
+  const age = 22;
+
+  return `${name} is ${age} and lives in ${city}`;
+}
+
+console.log(getPersonInfo());
