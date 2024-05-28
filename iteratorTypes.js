2606d6e2171f002b660a284b308975943d03676a
Dylan Palmboom <nofxboy1234@gmail.com>
2024-05-28 15:41:17 +0200

    Update iteratorTypes

--- iteratorTypes.js
+++ iteratorTypes.js
@@ -1,24 +1,31 @@
 /* eslint-disable */
 
 const array = ['✨', '💕', '🍃'];
-console.log(`array: ${array[Symbol.iterator]}`);
+// console.log(`array: ${array[Symbol.iterator]}`);
 
 const string = 'I love JavaScript!';
-console.log(`string: ${string[Symbol.iterator]}`);
+// console.log(`string: ${string[Symbol.iterator]}`);
 
 const object = { name: 'Lydia Hallie' };
-console.log(`object: ${object[Symbol.iterator]}`);
+object[Symbol.iterator] = function* () {
+  yield this;
+};
+// console.log(`object: ${object[Symbol.iterator]}`);
+// console.log([...object]);
+for (let item of object) {
+  console.log(item);
+}
 
 function regularFunction() {
   return 'I am a basic function';
 }
-console.log(`regularFunction: ${regularFunction[Symbol.iterator]}`);
+// console.log(`regularFunction: ${regularFunction[Symbol.iterator]}`);
 
 function* generatorFunction() {
   return 'I am a generator function';
 }
 
 const generatorObject = generatorFunction();
-console.log(`generatorObject: ${array[Symbol.iterator]}`);
+// console.log(`generatorObject: ${array[Symbol.iterator]}`);
 
 console.log('end');
