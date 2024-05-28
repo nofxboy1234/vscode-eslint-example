7e8f31f5215c90339b589783e32baf9776bcc21e
Dylan Palmboom <nofxboy1234@gmail.com>
2024-05-28 11:27:21 +0200

    Add prototypalInheritance.js

---
+++ prototypalInheritance.js
@@ -0,0 +1,22 @@
+/* eslint-disable */
+function Dog(name, breed, color) {
+  this.name = name;
+  this.breed = breed;
+  this.color = color;
+  this.bark = function () {
+    console.log('Woof!');
+  };
+}
+
+// console.log(Dog);
+// console.log('\n');
+// console.log(Dog.prototype);
+// console.log('\n');
+// console.log(Dog.prototype.constructor);
+// console.log('\n');
+
+const dog1 = new Dog('Daisy', 'Labrador', 'black');
+console.log(dog1);
+const dog2 = new Dog('Jack', 'Jack Russell', 'white');
+
+console.log('end');
