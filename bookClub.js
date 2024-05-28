8bf75bf8d2ebfc62669b57cff9c93f08f931d393
Dylan Palmboom <nofxboy1234@gmail.com>
2024-05-28 16:51:20 +0200

    Add bookClub.js

---
+++ bookClub.js
@@ -0,0 +1,53 @@
+/* eslint-disable */
+const bookClubs = [
+  {
+    name: 'The Cool Club',
+    clubMembers: [
+      {
+        name: 'John Doe',
+        books: [
+          { id: 'hs891', tite: 'A Perfect' },
+          { id: 'ey812', title: 'A Good Book' },
+        ],
+      },
+    ],
+  },
+  {
+    name: 'The Better Club',
+    clubMembers: [
+      {
+        name: 'Jane Doe',
+        books: [
+          { id: 'u8291', tite: 'A Great Book' },
+          { id: 'p9201', title: 'A Nice Book' },
+        ],
+      },
+    ],
+  },
+];
+
+function* iterateBooks(books) {
+  for (let i = 0; i < books.length; i++) {
+    yield books[i];
+  }
+}
+
+function* iterateMembers(members) {
+  for (let i = 0; i < members.length; i++) {
+    const clubMember = members[i];
+    yield* iterateBooks(clubMember.books);
+  }
+}
+
+function* iterateBookClubs(clubs) {
+  for (let i = 0; i < clubs.length; i++) {
+    const bookClub = clubs[i];
+    yield* iterateMembers(bookClub.clubMembers);
+  }
+}
+
+const it = iterateBookClubs(bookClubs);
+console.log(it.next());
+console.log(it.next());
+
+console.log('end');
