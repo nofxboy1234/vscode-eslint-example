bc1b7ca4ce8dc7f010882e15edda011b0a16f960
Dylan Palmboom <nofxboy1234@gmail.com>
2024-05-28 09:41:27 +0200

    Add hoisting.js

---
+++ hoisting.js
@@ -0,0 +1,19 @@
+/* eslint-disable */
+console.log(sum(2, 3));
+console.log(city);
+console.log(cat);
+console.log(name);
+
+function sum(x, y) {
+  return x + y;
+}
+
+const name = 'Lydia Hallie';
+const cat = 'whiskey';
+
+let info = {
+  age: 21,
+  nationality: 'Dutch',
+};
+
+var city = 'San Francisco';
