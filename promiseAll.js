fe50c83cfadc4dd07e28f314c2cb301235ea5311
Dylan Palmboom <nofxboy1234@gmail.com>
2024-05-29 13:13:56 +0200

    Update urls to use jsonplaceholder.typicode.com

--- promiseAll.js
+++ promiseAll.js
@@ -2,16 +2,19 @@
 // `request(..)` is a Promise-aware Ajax utility,
 // like we defined earlier in the chapter
 
-var p1 = fetch('https://google.com/');
-var p2 = fetch('https://twitter.com/');
+var p1 = fetch('https://jsonplaceholder.typicode.com/todos/1');
+var p2 = fetch('https://jsonplaceholder.typicode.com/todos/2');
 
 Promise.all([p1, p2])
   .then(function (msgs) {
     // both `p1` and `p2` fulfill and pass in
     // their messages here
     console.log(msgs);
-    return fetch('https://www.theodinproject.com/');
+    return fetch('https://jsonplaceholder.typicode.com/todos/3');
   })
   .then(function (msg) {
+    return msg.json();
+  })
+  .then((msg) => {
     console.log(msg);
   });
--- promiseRace.js
+++ promiseRace.js
@@ -2,14 +2,15 @@
 // `request(..)` is a Promise-aware Ajax utility,
 // like we defined earlier in the chapter
 
-var p1 = fetch('https://google.com/');
-var p2 = fetch('https://twitter.com/');
+var p1 = fetch('https://jsonplaceholder.typicode.com/todos/1');
+var p2 = fetch('https://jsonplaceholder.typicode.com/todos/2');
 
 Promise.race([p1, p2])
   .then(function (msg) {
     // either `p1` or `p2` will win the race
     console.log(msg);
-    return fetch('https://www.theodinproject.com/');
+    return msg.json();
+    // return fetch('https://www.theodinproject.com/');
   })
   .then(function (msg) {
     console.log(msg);
