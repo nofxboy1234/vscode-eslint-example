d7222be50a610488fc7292cffaf76ce80f264938
Dylan Palmboom <nofxboy1234@gmail.com>
2024-05-29 13:03:48 +0200

    Update urls

--- promiseAll.js
+++ promiseAll.js
@@ -2,15 +2,15 @@
 // `request(..)` is a Promise-aware Ajax utility,
 // like we defined earlier in the chapter
 
-var p1 = fetch('https://www.google.com/');
-var p2 = fetch('https://www.google.com/');
+var p1 = fetch('https://google.com/');
+var p2 = fetch('https://google.com/');
 
 Promise.all([p1, p2])
   .then(function (msgs) {
     // both `p1` and `p2` fulfill and pass in
     // their messages here
     console.log(msgs);
-    return fetch('https://www.google.com/');
+    return fetch('https://google.com/');
   })
   .then(function (msg) {
     console.log(msg);
--- promiseRace.js
+++ promiseRace.js
@@ -2,14 +2,14 @@
 // `request(..)` is a Promise-aware Ajax utility,
 // like we defined earlier in the chapter
 
-var p1 = fetch('https://www.google.com/');
-var p2 = fetch('https://www.google.com/');
+var p1 = fetch('https://google.com/');
+var p2 = fetch('https://google.com/');
 
 Promise.race([p1, p2])
   .then(function (msg) {
     // either `p1` or `p2` will win the race
-    console.log(msg)
-    return fetch('https://www.google.com/');
+    console.log(msg);
+    return fetch('https://google.com/');
   })
   .then(function (msg) {
     console.log(msg);
