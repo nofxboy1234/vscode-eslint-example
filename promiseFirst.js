f15853b811c3a3aaf5019d5b1ef602a8d8eed61d
Dylan Palmboom <nofxboy1234@gmail.com>
2024-05-29 13:40:30 +0200

    Add promiseFirst

---
+++ promiseFirst.js
@@ -0,0 +1,36 @@
+/* eslint-disable */
+// polyfill-safe guard check
+if (!Promise.first) {
+  Promise.first = function (prs) {
+    return new Promise(function (resolve, reject) {
+      // loop through all promises
+      prs.forEach(function (pr) {
+        // normalize the value
+        Promise.resolve(pr)
+          // whichever one fulfills first wins, and
+          // gets to resolve the main promise
+          .then(resolve);
+      });
+    });
+  };
+}
+
+function foo() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('foo() won the race!');
+    }, 2000);
+  });
+}
+
+function bar() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('bar() won the race!');
+    }, 1000);
+  });
+}
+
+Promise.first([foo(), bar()]).then((result) => {
+  console.log(result);
+});
