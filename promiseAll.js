feaa5d5961547bf09aa3fe1bf251fac40df06bb1
Dylan Palmboom <nofxboy1234@gmail.com>
2024-05-29 13:08:25 +0200

    Update urls

--- promiseAll.js
+++ promiseAll.js
@@ -3,14 +3,14 @@
 // like we defined earlier in the chapter
 
 var p1 = fetch('https://google.com/');
-var p2 = fetch('https://google.com/');
+var p2 = fetch('https://twitter.com/');
 
 Promise.all([p1, p2])
   .then(function (msgs) {
     // both `p1` and `p2` fulfill and pass in
     // their messages here
     console.log(msgs);
-    return fetch('https://google.com/');
+    return fetch('https://www.theodinproject.com/');
   })
   .then(function (msg) {
     console.log(msg);
--- promiseRace.js
+++ promiseRace.js
@@ -3,13 +3,13 @@
 // like we defined earlier in the chapter
 
 var p1 = fetch('https://google.com/');
-var p2 = fetch('https://google.com/');
+var p2 = fetch('https://twitter.com/');
 
 Promise.race([p1, p2])
   .then(function (msg) {
     // either `p1` or `p2` will win the race
     console.log(msg);
-    return fetch('https://google.com/');
+    return fetch('https://www.theodinproject.com/');
   })
   .then(function (msg) {
     console.log(msg);
