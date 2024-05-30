6526692e9bbedf60f6f788cf7409ed3c8eb14787
Dylan Palmboom <nofxboy1234@gmail.com>
2024-05-30 19:44:24 +0200

    Add Lydia's Youtube comment regarding chained .thens

--- promiseChaining.js
+++ promiseChaining.js
@@ -1,4 +1,16 @@
 /* eslint-disable */
+
+/*
+The first promise constructor resolves, the first then handler is encountered, 
+this handler is immediately added to the microtask queue. 
+However, the callstack isn't empty at this point because there are more thens 
+to handle. So it keeps creating Promise Reaction/Promise Object records for 
+the subsequent thens and these are added to the fulfill reactions list, 
+but they aren't fulfilled at this point since the first handler is still 
+waiting on the microtask queue. Only when the callstack is empty would this 
+resolve and add the next then's handler to the microtask queue.
+*/
+
 new Promise((resolve, reject) => {
   resolve(1);
 })
