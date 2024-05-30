fbe49da6b2c88ca4bcca843aff13cdd2278da812
Dylan Palmboom <nofxboy1234@gmail.com>
2024-05-30 13:41:14 +0200

    Add promiseUncancelable

---
+++ promiseUncancelable.js
@@ -0,0 +1,26 @@
+/* eslint-disable */
+function timeoutPromise(delay) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      reject('Timeout!');
+    }, delay);
+  });
+}
+
+function doSomething(msg) {
+  console.log(msg);
+}
+
+function handleError(err) {
+  console.error(err);
+}
+
+// var p = foo(42);
+var p = Promise.resolve(42);
+
+Promise.race([p, timeoutPromise(3000)]).then(doSomething, handleError);
+
+p.then(function (msg) {
+  // still happens even in the timeout case :(
+  console.log(msg);
+});
