28b481fb80226bf12f9c9e60b119648050cc51e2
Dylan Palmboom <nofxboy1234@gmail.com>
2024-05-30 21:35:52 +0200

    Refactor promiseChaining

--- promiseChaining.js
+++ promiseChaining.js
@@ -1,10 +1,12 @@
 /* eslint-disable */
 
 /*
-The first promise constructor resolves, the first then handler is encountered, 
-this handler is immediately added to the microtask queue. 
+The first promise constructor resolves, 
+the first then handler is encountered: 
+  - this handler is immediately added to the microtask queue. 
 However, the callstack isn't empty at this point because there are more thens 
-to handle. So it keeps creating Promise Reaction/Promise Object records for 
+to handle. 
+So it keeps creating Promise Reaction/Promise Object records for 
 the subsequent thens and these are added to the fulfill reactions list, 
 but they aren't fulfilled at this point since the first handler is still 
 waiting on the microtask queue. Only when the callstack is empty would this 
@@ -14,9 +16,15 @@ resolve and add the next then's handler to the microtask queue.
 new Promise((resolve, reject) => {
   resolve(1);
 })
-  .then((result) => result * 2)
-  .then((result) => result * 2)
-  .then((result) => console.log(result));
+  .then((result) => {
+    return result * 2;
+  })
+  .then((result) => {
+    return result * 2;
+  })
+  .then((result) => {
+    console.log(result);
+  });
 
 // async function hello() {
 //   //
