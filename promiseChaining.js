165fa1b96d8a83f08a0a8b2fd9730a77a0a7cbcf
Dylan Palmboom <nofxboy1234@gmail.com>
2024-05-30 20:40:17 +0200

    Remove one of the two youtube links in the comment

--- promiseChaining.js
+++ promiseChaining.js
@@ -10,7 +10,6 @@ but they aren't fulfilled at this point since the first handler is still
 waiting on the microtask queue. Only when the callstack is empty would this 
 resolve and add the next then's handler to the microtask queue.
 
-https://youtu.be/Xs1EMmBLpn4?list=PLaYco3qR_dVJm_clCOg-B4KXhPsonE7hx&t=108
 https://youtu.be/Xs1EMmBLpn4?list=PLaYco3qR_dVJm_clCOg-B4KXhPsonE7hx&t=417
 */
 
