f01c3d737654d7608aef956e5ae6869ececafb2d
Dylan Palmboom <nofxboy1234@gmail.com>
2024-05-30 21:47:09 +0200

    Update step-by-step comments and save Promises into variables at each step of the Promise chain

--- promiseChaining.js
+++ promiseChaining.js
@@ -7,24 +7,25 @@ the first then handler is encountered:
 However, the callstack isn't empty at this point because there are more thens 
 to handle. 
 So it keeps creating Promise Reaction/Promise Object records for 
-the subsequent thens and these are added to the fulfill reactions list, 
+the subsequent thens and these are added to the fulfill reactions list,
+[inside the Promise that the .then is attached to] 
 but they aren't fulfilled at this point since the first handler is still 
 waiting on the microtask queue. Only when the callstack is empty would this 
 resolve and add the next then's handler to the microtask queue.
 */
 
-new Promise((resolve, reject) => {
+const p = new Promise((resolve, reject) => {
   resolve(1);
-})
-  .then((result) => {
-    return result * 2;
-  })
-  .then((result) => {
-    return result * 2;
-  })
-  .then((result) => {
-    console.log(result);
-  });
+});
+const p2 = p.then((result) => {
+  return result * 2;
+});
+const p3 = p2.then((result) => {
+  return result * 2;
+});
+const p4 = p3.then((result) => {
+  console.log(result);
+});
 
 // async function hello() {
 //   //
