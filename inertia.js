5ce8de0249ad588881bf1e207f7aa5675ac12804
Dylan Palmboom <nofxboy1234@gmail.com>
2024-05-30 09:57:34 +0200

    Add inertia

---
+++ inertia.js
@@ -0,0 +1,34 @@
+/* eslint-disable */
+function foo(x, y, cb) {
+  ajax('http://some.url.1/?x=' + x + '&y=' + y, cb);
+}
+j
+foo(11, 31, function (err, text) {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(text);
+  }
+});
+
+// polyfill-safe guard check
+if (!Promise.wrap) {
+  Promise.wrap = function (fn) {
+    return function () {
+      var args = [].slice.call(arguments);
+
+      return new Promise(function (resolve, reject) {
+        fn.apply(
+          null,
+          args.concat(function (err, v) {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(v);
+            }
+          }),
+        );
+      });
+    };
+  };
+}
