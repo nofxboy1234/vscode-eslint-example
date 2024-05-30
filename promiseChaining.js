de09278fc8fab49b10910592597105985090b3f3
Dylan Palmboom <nofxboy1234@gmail.com>
2024-05-30 16:34:57 +0200

    Add promiseChaining

---
+++ promiseChaining.js
@@ -0,0 +1,7 @@
+/* eslint-disable */
+new Promise((resolve, reject) => {
+  resolve(1);
+})
+  .then((result) => result * 2)
+  .then((result) => result * 2)
+  .then((result) => console.log(result));
