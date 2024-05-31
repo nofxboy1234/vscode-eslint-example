e804fe5d5b642f6bb37a56d1e5b9a7fa1d3660e9
Dylan Palmboom <nofxboy1234@gmail.com>
2024-05-31 16:20:47 +0200

    Update microTasks

--- microTasks.js
+++ microTasks.js
@@ -1,8 +1,14 @@
+function asyncStuff() {
+  console.log('async: Stuff!');
+}
+
 function hello() {
   return new Promise((resolve, reject) => {
-    console.log('Starting hello promise');
+    console.log('sync: Starting hello promise');
+    queueMicrotask(asyncStuff);
+
     // resolve('hello promise fulfilled!');
-    reject('hello promise rejected!')
+    reject(Error('hello promise rejected!'));
   });
 }
 
@@ -15,3 +21,5 @@ function rejected(err) {
 }
 
 hello().then(fulfilled).catch(rejected);
+
+console.log('end');
