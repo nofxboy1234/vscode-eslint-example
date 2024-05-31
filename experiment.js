e9595521c4f2ede9b1e606732233fe94ac2898ae
Dylan Palmboom <nofxboy1234@gmail.com>
2024-05-31 13:34:45 +0200

    Update

--- experiment.js
+++ experiment.js
@@ -1,9 +1,14 @@
 /* eslint-disable */
 function hello() {
   return new Promise((resolve, reject) => {
-    fetch('https://skjdfhdskjaf.com').catch((err) => {
-      reject(err);
-    });
+    fetch('https://google.com')
+      // fetch('https://googleeeeeeee.com')
+      .then((result) => {
+        resolve(result);
+      })
+      .catch((err) => {
+        reject(err);
+      });
   });
 }
 
@@ -16,3 +21,24 @@ hello()
   });
 
 console.log('end');
+
+// ***************************************
+// async function hello() {
+//   try {
+//     await fetch('https://google.com');
+//     // await fetch('https://googleeeeeeee.com');
+//     return result;
+//   } catch (err) {
+//     throw err;
+//   }
+// }
+
+// hello()
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch((err) => {
+//     console.error(err);
+//   });
+
+// console.log('end');
