2ba43407d3589f0a45d85c37b49a456116f32b64
Dylan Palmboom <nofxboy1234@gmail.com>
2024-05-31 15:28:13 +0200

    Add executionContexts

---
+++ executionContexts.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+const firstName = 'Lydia';
+let lastName = 'Hallie';
+
+function greet(nameToGreet) {
+  const fullName = nameToGreet + ' ' + lastName;
+  return 'Hello, ' + fullName;
+}
+
+greet(firstName);
