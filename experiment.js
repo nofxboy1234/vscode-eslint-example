a1037e3e024ef091a37afd9bf9f795a4c42576b0
Dylan Palmboom <nofxboy1234@gmail.com>
2024-05-31 13:45:25 +0200

    Update experiment

--- experiment.js
+++ experiment.js
@@ -1,24 +1,20 @@
 /* eslint-disable */
-function hello() {
-  return new Promise((resolve, reject) => {
-    fetch('https://google.com')
-      // fetch('https://googleeeeeeee.com')
-      .then((result) => {
-        resolve(result);
-      })
-      .catch((err) => {
-        reject(err);
-      });
-  });
+async function hello() {
+  try {
+    // await fetch('https://google.com');
+    await fetch('https://googleeeeeeee.com')
+    return 'done!';
+  } catch (err) {
+    throw err;
+  }
 }
 
-hello()
-  .then((result) => {
-    console.log(result);
-  })
-  .catch((err) => {
-    console.error(err);
-  });
+const p = hello();
+p.then((result) => {
+  console.log(result);
+}).catch((err) => {
+  console.error(err);
+});
 
 console.log('end');
 
