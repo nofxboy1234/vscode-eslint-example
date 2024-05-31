eb12fef98a9a84233c2e237bf307b8cc2c02e886
Dylan Palmboom <nofxboy1234@gmail.com>
2024-05-31 16:28:09 +0200

    Refactor microTasks

--- microTasks.js
+++ microTasks.js
@@ -1,12 +1,17 @@
 function asyncStuff() {
   console.log('async: Stuff!');
+  return 'asyncStuff result';
 }
 
 function hello() {
   return new Promise((resolve, reject) => {
     console.log('sync: Starting hello promise');
 
-    Promise.resolve().then(asyncStuff);
+    Promise.resolve()
+      .then(asyncStuff)
+      .then((result) => {
+        console.log(result);
+      });
     // queueMicrotask(asyncStuff);
 
     // resolve('hello promise fulfilled!');
