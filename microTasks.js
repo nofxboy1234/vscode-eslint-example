69d0b09c3331b70f86484a1e17ba775ad5d49d6d
Dylan Palmboom <nofxboy1234@gmail.com>
2024-06-01 13:01:11 +0200

    Refactor microTasks

--- microTasks.js
+++ microTasks.js
@@ -1,41 +1,50 @@
-async function test() {
-  await queueMicrotask(() => {
-    console.log('1');
-    queueMicrotask(() => {
-      console.log('2');
-    });
-  });
-
-  console.log('3');
-}
-test();
-
-// function asyncStuff() {
-//   console.log('async: Stuff!');
-//   return 'asyncStuff result';
-// }
-
-// function hello() {
-//   return new Promise((resolve, reject) => {
-//     console.log('sync: Starting hello promise');
-
-//     Promise.resolve()
-//       .then(asyncStuff)
-//       .then((result) => {
-//         console.log(result);
-//         resolve('hello promise fulfilled!');
-//       });
+// async function test() {
+//   await queueMicrotask(() => {
+//     console.log('1');
+//     queueMicrotask(() => {
+//       console.log('2');
+//     });
 //   });
-// }
 
-// function fulfilled(result) {
-//   console.log(result);
+//   console.log('3');
 // }
+// test();
 
-// function rejected(err) {
-//   console.error(err);
-// }
+function asyncStuff() {
+  console.log('async: Stuff!');
+  return 'asyncStuff result';
+}
+
+async function asyncHello() {
+  console.log('sync: Starting hello promise');
+
+  const result = await queueMicrotask(asyncStuff);
+  console.log(result);
+  return 'hello promise fulfilled!';
+}
+
+function hello() {
+  return new Promise((resolve, reject) => {
+    console.log('sync: Starting hello promise');
+
+    Promise.resolve()
+      .then(asyncStuff)
+      .then((result) => {
+        console.log(result);
+        resolve('hello promise fulfilled!');
+      });
+  });
+}
+
+function fulfilled(result) {
+  console.log(result);
+}
+
+function rejected(err) {
+  console.error(err);
+}
 
 // hello().then(fulfilled).catch(rejected);
+asyncHello().then(fulfilled).catch(rejected);
 
-// console.log('end');
+console.log('end');
