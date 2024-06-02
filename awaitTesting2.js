73ac92ed594c09cb9e7aa2ac86e65c7396e15810
Dylan Palmboom <nofxboy1234@gmail.com>
2024-06-02 19:27:06 +0200

    Update

--- awaitTesting2.js
+++ awaitTesting2.js
@@ -1,8 +1,9 @@
 async function hello() {
   try {
     // const result = await 22;
+    const result = await 'async';
     // const result = await Promise.resolve(22);
-    const result = await Promise.reject('error!');
+    // const result = await Promise.reject('error!');
     console.log(result);
   } catch (err) {
     console.error(err);
@@ -10,3 +11,4 @@ async function hello() {
 }
 
 hello();
+console.log('sync');
